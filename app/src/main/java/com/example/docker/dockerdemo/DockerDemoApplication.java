3080e0187c134ee2a3bea8147f4e67872b0f4b8d
shiyanwu <yanwu.shi@ehking.com>
2018-07-04 15:43:19 +0800

    add comment

--- app/src/main/java/com/example/docker/dockerdemo/DockerDemoApplication.java
+++ app/src/main/java/com/example/docker/dockerdemo/DockerDemoApplication.java
@@ -3,6 +3,9 @@ package com.example.docker.dockerdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author shiyanwu
+ */
 @SpringBootApplication
 public class DockerDemoApplication {
 
